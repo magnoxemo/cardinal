22d85463a993879a2a070ec4fd6bb188e72a30c2
magnoxemo <walid.ahammed01@gmail.com>
2025-07-21 16:55:05 -0500

    adding the base structure. But I need to explore what that pointValue method does under the hood

--- include/auxkernels/DiscrepencyAux.h
+++ include/auxkernels/DiscrepencyAux.h
@@ -1,8 +1,38 @@
-//
-// Created by ebny_walid on 7/21/25.
-//
+#pragma once
+#include "AuxKernel.h"
+//forward declaration
+class SolutionUserObjectBase;
 
-#ifndef CARDINAL_DISCREPENCYAUX_H
-#define CARDINAL_DISCREPENCYAUX_H
+class DiscrepencyAux : public AuxKernel
+{
+public:
+  static InputParameters validParams();
 
-#endif // CARDINAL_DISCREPENCYAUX_H
+  DiscrepencyAux(const InputParameters & parameters);
+  virtual void initialSetup() override;
+
+protected:
+
+  virtual Real computeValue() override;
+
+  /// Reference to the SolutionUserObject storing the solution
+  const SolutionUserObjectBase & _solution_object;
+
+  Real getBaseVariableValueByElement(libMesh::Elem* element) const;
+
+  /// The variable name of interest
+  std::string _mesh_amalgamation_var_name;
+
+  AuxVariableName & _base_variable_name;
+  MooseVariableBase &  _base_variable;
+
+  ///AuxiliarySystem reference
+  AuxiliarySystem & _auxiliary_system;
+
+  ///DOF map
+  libMesh::DofMap & _dof_map;
+
+  ///Metric variable index
+  const unsigned int _base_variable_index;
+
+};
--- src/auxkernels/DiscrepencyAux.C
+++ src/auxkernels/DiscrepencyAux.C
@@ -1,3 +1,63 @@
-//
-// Created by ebny_walid on 7/21/25.
-//
+#include "DiscrepenyAux.h"
+#include "MooseError.h"
+#include "SolutionUserObjectBase.h"
+
+registerMooseObject("DiscrepencyAux", CardinalApp);
+
+InputParameters
+DiscrepencyAux::validParams(){
+  InputParameters params = AuxKernel::validParams();
+  params.addRequiredParam<VariableName>("base_variable_name","name of the variable which's relative"
+                                                              " difference will be calculated");
+  params.addClassDescription("calculates the relative difference between two solution field.");
+  params.addRequiredParam<UserObjectName>("solution", "The name of the SolutionUserObject");
+  params.addParam<std::string>("from_variable","The name of the variable to extract from the file");
+  return params;
+}
+
+DiscrepencyAux::DiscrepencyAux(const InputParameters & parameters)
+  : AuxKernel(parameters),
+    _solution_object(getUserObject<SolutionUserObjectBase>("solution")),
+    _base_variable_name(getParam<AuxVariableName>("metric_variable_name")),
+    _base_variable(_fe_problem.getVariable(_tid, _base_variable_name)),
+    _auxiliary_system(_fe_problem.getAuxiliarySystem()),
+    _dof_map(_auxiliary_system.dofMap()),
+    _base_variable_index(_auxiliary_system.getVariable(_tid, _base_variable_name).number())
+
+{
+}
+
+void
+DiscrepencyAux::initialSetup()
+{
+  // If 'from_variable' is supplied, use the value
+  if (isParamValid("from_variable"))
+    _var_name = getParam<std::string>("from_variable");
+  else
+    //throw a moose error
+    mooseError("We need the mesh_amalgamation score");
+}
+
+Real
+DiscrepencyAux::computeValue()
+{
+  //get the score from the amr mesh
+  //amr_score = get the solution from the mesh
+  // The value to output
+  Real base_variable_value = getBaseVariableValueByElement(_current_elem);
+  Real mesh_amalgamtion_output = _solution_object.pointValue(_t, _current_elem->vertex_average(), _var_name);
+  /*our main investigation will be how that point value get projected*/
+
+  return (base_variable_value - mesh_amalgamation_output)/base_variable_value ;
+}
+
+Real
+DiscrepencyAux::getBaseVariableValueByElement(const libMesh::Elem * elem) const
+{
+  std::vector<libMesh::dof_id_type> dof_indices;
+  std::vector<double> solution_value(1);
+  _dof_map.dof_indices(elem, dof_indices, _metric_variable_index);
+  _auxiliary_system.solution().get(dof_indices, solution_value);
+
+  return solution_value[0];
+}
